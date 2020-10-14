6c9b0db54da2bafeafb2336c3b70dcb49275ee80
BaroqBard <theodore.m.cheek@gmail.com>
2020-10-14 17:11:06 -0400

    more cleaning, less loops, separate next

--- cleaning.R
+++ cleaning.R
@@ -1,43 +1,71 @@
-library(stringr)
+#### Clean & Prepare Rich Gun Data ####
+# The participant categories are especially rich dictionaries
+# The goal here is to expand the data set to account for
+# Participants separately
+library(dplyr)
+library(tidyr)
 
-gun.orig = read.csv('gun-violence-data_01-2013_03-2018.csv')
+gun.orig = read.csv('gun-violence-data_01-2013_03-2018.csv', 
+                    stringsAsFactors = F)
 
-
-#make empty dataframe for appending
-gun.df = data.frame(Incident_ID = character(),
-                    Participant_Type = character())
 
 "
 ####Participant Data to Clean####
+---------------------------------
+Incident ID
+Participant Type
 Participant Age
 Participant Age Group
 Participant Gender
 Participant Status
-Participant Type
 "
 
-
-strcleaning = function (x){
-  #takes in factor
-  x = gsub("\\|", " ", x)
-  x = str_squish(x)
-  x = strsplit(x, " ")
+#takes in character, returns vector of chars
+char.cleaner = function (x) {
+  require(stringr)
   
-  #returns list of strings
-  return(x)
+  if (is.null(x)) {
+    return("N/A")
+    
+  } else {
+    x = gsub(" ", "", x)
+    x = gsub("\\|", " ", x)
+    x = str_squish(x)
+    x = strsplit(x, " ")
+    
+    return(x)
+  }
 }
 
-for (i in 1:length(gun.orig$incident_id)){
-  #clean factors into list of strings
-  
-  #make a matrix, id x cleanstrings
-  
-  #append to dataframe
-  do.call(rbind.data.frame, )
-  
-}
+#tidyr chop???? preserve width, lengthen dataframe, works on lists
+
+gun.orig$participant_type = lapply(gun.orig$participant_type, 
+                                  char.cleaner)
+gun.orig$participant_age = lapply(gun.orig$participant_age, 
+                                  char.cleaner)
+gun.orig$participant_age_group = lapply(gun.orig$participant_age_group, 
+                                  char.cleaner)
+gun.orig$participant_gender = lapply(gun.orig$participant_gender, 
+                                  char.cleaner)
+gun.orig$participant_status = lapply(gun.orig$participant_status,
+                                  char.cleaner)
+
+#note for next time: add in and work on separators for this sect
+# sep into key columns, join, select out
 
 
-
-
-t.var = gun.orig$participant_type[1]
+part.type = gun.orig %>% 
+  select(., incident_id, participant_type) %>% 
+  unchop(., participant_type)
+part.age = gun.orig %>% 
+  select(., incident_id, participant_age) %>% 
+  unchop(., participant_age)
+part.age_group = gun.orig %>% 
+  select(., incident_id, participant_age_group) %>% 
+  unchop(., participant_age_group)
+part.gender = gun.orig %>% 
+  select(., incident_id, participant_gender) %>% 
+  unchop(., participant_gender)
+part.status = gun.orig %>% 
+  select(., incident_id, participant_status) %>% 
+  unchop(., participant_status)
